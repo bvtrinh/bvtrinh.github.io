02f5559a412be71f587459089e2a8e5d3131ccbf
Tyler Trinh <tylervtrinh@gmail.com>
2021-03-03 23:28:11 -0800

    docs: add skill logo images

---
+++ src/static/skillLogos.ts
@@ -0,0 +1,15 @@
+import typescript from "../images/logos/typescript.svg";
+import nodejs from "../images/logos/nodejs.svg";
+import mongodb from "../images/logos/mongodb.svg";
+import react from "../images/logos/react.svg";
+import chakra from "../images/logos/chakra.svg";
+import selenium from "../images/logos/selenium.svg";
+
+export const SKILL_LOGOS: { [key: string]: any } = {
+  typescript,
+  nodejs,
+  mongodb,
+  react,
+  chakra,
+  selenium,
+};
