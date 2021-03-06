2105f5778c601c5cd2ba5126ae50338ba97ab72c
Tyler Trinh <tylervtrinh@gmail.com>
2021-03-05 22:34:18 -0800

    docs: update skill logos

--- src/static/skillLogos.ts
+++ src/static/skillLogos.ts
@@ -1,15 +1,41 @@
+import python from "../images/logos/typescript.svg";
 import typescript from "../images/logos/typescript.svg";
+import cplusplus from "../images/logos/c++.svg";
 import nodejs from "../images/logos/nodejs.svg";
-import mongodb from "../images/logos/mongodb.svg";
+import php from "../images/logos/php.svg";
 import react from "../images/logos/react.svg";
-import chakra from "../images/logos/chakra.svg";
+import jest from "../images/logos/jest.svg";
 import selenium from "../images/logos/selenium.svg";
+import codeigniter from "../images/logos/codeigniter.svg";
+import bootstrap from "../images/logos/bootstrap.svg";
+import chakra from "../images/logos/chakra.svg";
+import html from "../images/logos/html.svg";
+import css from "../images/logos/css.svg";
+import mongodb from "../images/logos/mongodb.svg";
+import postgres from "../images/logos/postgresql.svg";
+import docker from "../images/logos/docker.svg";
+import gcp from "../images/logos/gcp.svg";
+import heroku from "../images/logos/heroku.svg";
+import github_actions from "../images/logos/github_actions.svg";
 
 export const SKILL_LOGOS: { [key: string]: any } = {
+  python,
   typescript,
+  cplusplus,
   nodejs,
-  mongodb,
+  php,
   react,
-  chakra,
+  jest,
   selenium,
+  codeigniter,
+  bootstrap,
+  chakra,
+  html,
+  css,
+  mongodb,
+  postgres,
+  docker,
+  gcp,
+  heroku,
+  github_actions,
 };
