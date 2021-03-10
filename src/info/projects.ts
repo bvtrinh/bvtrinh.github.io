b3110bfe2220018030f14cacedfce0f213ad71b8
Tyler Trinh <tylervtrinh@gmail.com>
2021-03-09 22:51:15 -0800

    docs: use old sparklist info

--- src/info/projects.ts
+++ src/info/projects.ts
@@ -9,15 +9,6 @@ export interface ProjectProps {
 
 export const PROJECTS: ProjectProps[] = [
   {
-    title: "Sparklist",
-    date: "Ongoing",
-    skills: ["typescript", "nodejs", "mongodb", "react", "chakra", "selenium"],
-    about: "A web app to share wishlists with friends and family.",
-    description:
-      "The main idea was derived from myself not knowing what to gifts to get for birthdays and Christmas. Some of the features are inspired by pcpartpicker.com. It originally started as a class project back in Feb 2020 but it was idea of mine so I’ve picked it back up again in Feb 2021. The next version I plan to use a more modern stack and try to use less API’s since I was using free trials.",
-    links: { github: "https://github.com/bvtrinh/sparklist" },
-  },
-  {
     title: "Tokimons",
     date: "Jan 2021",
     skills: [
@@ -48,6 +39,15 @@ export const PROJECTS: ProjectProps[] = [
     links: {
       github: "https://github.com/dchop/Reversi-MCTS",
     },
+  },
+  {
+    title: "Sparklist",
+    date: "Apr 2020",
+    skills: ["expressjs", "nodejs", "mongodb", "bootstrap", "gcp", "nginx"],
+    about: "A web app to share wishlists with friends and family.",
+    description:
+      "The main idea was derived from myself not knowing what to gifts to get for birthdays and Christmas. Some of the features are inspired by pcpartpicker.com. It originally started as a class project back in Feb 2020 but it was idea of mine so I’ve picked it back up again in Feb 2021. The next version I plan to use a more modern stack and try to use less API’s since I was using free trials.",
+    links: { github: "https://github.com/bvtrinh/sparklist" },
   },
   {
     title: "Avise",
